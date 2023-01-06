130590876ad2452f93346f87764f23bc1b8d4b96
kekelberi <alexei.gaponov@gmail.com>
2023-01-05 22:29:18 -0600

    save

--- main.go
+++ main.go
@@ -1,8 +1,21 @@
 package main
 
-import "log"
+import (
+	"log"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+
+)
 
 func main() {
 	log.Println("start")
+	e := echo.New()
 
+	e.GET("/", hello)
+	e.Logger.Fatal(e.Start(":3000"))
+}
+
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "hi")
 }

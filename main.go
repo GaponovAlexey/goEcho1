b7075912e636cf8f0552cb9dcd879ed5a1978fd2
kekelberi <alexei.gaponov@gmail.com>
2023-01-08 16:27:48 -0600

    save

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"log"
 	h "net/http"
 	"strconv"
 
@@ -25,46 +24,41 @@ func (p *ProdVal) Validate(i interface{}) error {
 }
 
 func main() {
-	log.Println("start")
-
 	//func
-	e.GET("/", hello)
-
 	e.GET("/p", func(c echo.Context) error {
 		return c.JSON(h.StatusOK, products)
 	})
-	//getId -- d
-	e.GET("/prod/:id", getId)
+	//getId -- id
+	e.GET("/p/:id", getId)
 	//POST
 	e.POST("/p", addProd)
+	e.PUT("/p/:id", putProd)
+	e.DELETE("/p/:id", delProd)
 
 	//end
 	e.Logger.Fatal(e.Start(":3000"))
 }
 
 // func
-func hello(c echo.Context) error {
-	return c.String(h.StatusOK, "hi")
-}
-
 func getId(c echo.Context) error {
-	paragraphID, err := strconv.Atoi(c.Param("id"))
+	pID, err := strconv.Atoi(c.Param("id"))
 
 	if err != nil {
 		return err
 	}
+
 	var newProduct map[int]string
+
 	for _, v := range products {
 		for k := range v {
-			if k == paragraphID {
+			if pID == k {
 				newProduct = v
 			}
 		}
 	}
 	if newProduct == nil {
-		return c.JSON(h.StatusNotFound, "not found")
+		return c.JSON(h.StatusNotFound, "not found product id")
 	}
-
 	return c.JSON(h.StatusOK, newProduct)
 }
 
@@ -73,7 +67,9 @@ func addProd(c echo.Context) error {
 		Name string `json:"product_name" validate:"required,min=4"`
 	}
 	var reqBody body
+
 	e.Validator = &ProdVal{val: v}
+
 	if err := c.Bind(&reqBody); err != nil {
 		return err
 	}
@@ -85,4 +81,70 @@ func addProd(c echo.Context) error {
 	}
 	products = append(products, product)
 	return c.JSON(h.StatusOK, product)
+}
+
+func putProd(c echo.Context) error {
+	pID, err := strconv.Atoi(c.Param("id"))
+	var prod map[int]string
+
+	if err != nil {
+		return err
+	}
+	for _, p := range products {
+		for k := range p {
+			if pID == k {
+				prod = p
+			}
+		}
+	}
+
+	if prod == nil {
+		return c.JSON(h.StatusNotFound, "not data")
+	}
+
+	type req struct {
+		Name string `json:"product_name" validate:"required,min=4"`
+	}
+
+	var body req
+
+	e.Validator = &ProdVal{val: v}
+
+	if err := c.Bind(&body); err != nil {
+		return err
+	}
+	if err := c.Validate(body); err != nil {
+		return err
+	}
+
+	prod[pID] = body.Name
+	return c.JSON(h.StatusOK, prod)
+}
+
+func delProd(c echo.Context) error {
+	var prod map[int]string
+	var index int
+	pID, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return err
+	}
+
+	for i, p := range products {
+		for k := range p {
+			if pID == k {
+				prod = p
+				index = i
+			}
+		}
+	}
+	if prod == nil {
+		return c.JSON(h.StatusNotFound, "not Found")
+	}
+	splice := func(s []map[int]string, index int) []map[int]string {
+		return append(s[:index], s[index+1:]...)
+	}
+	
+	products = splice(products, index)
+	
+	return c.JSON(h.StatusOK, prod)
 }

98a292b0ae0bbd0898336e369121c927c4a1e232
kekelberi <alexei.gaponov@gmail.com>
2023-01-12 18:46:40 -0600

    save

--- cmd/start/handler.go
+++ cmd/start/handler.go
@@ -5,25 +5,30 @@ import (
 	h "net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 
 )
 
 func serverMessage(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		log.Println("server MESSAGE")
+		// c.Request().URL.Path = "/api/1"
+		// fmt.Printf("%+v\n", c.Request())
 		return next(c)
 	}
 }
 
 func Start() {
 	//start
-	e.GET("/", func(c echo.Context) error {
+	e.Pre(middleware.RemoveTrailingSlash())
+	e.GET("/api", func(c echo.Context) error {
+		// fmt.Printf("inside = %+v\n", c.Request())
 		return c.JSON(h.StatusOK, object)
 	})
-	e.GET("/:id", getID, serverMessage)
-	e.POST("/", addObject)
-	e.PUT("/:id", putObject)
-	e.DELETE("/:id", delObject)
+	e.GET("/api/:id", getID)
+	e.POST("/api", addObject)
+	e.PUT("/api/:id", putObject)
+	e.DELETE("/api/:id", delObject, middleware.BodyLimit("1"))
 	//end
 	e.Logger.Fatal(e.Start(":3000"))
 
